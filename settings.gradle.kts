38143ae61e2dcbaff9ee6526d0c464ad369459e1
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-02-12 14:21:54 +0300

    Update dependencies versions (#12)
    * Set kotlin.version=1.9.22, spring-cloud.version=2023.0.0, spring-boot.version=3.2.2
    * Set Gradle version=8.6
    * Replace base image with eclipse-temurin:21-jdk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,6 @@ pluginManagement {
         kotlin("jvm") version (extra["kotlin.version"] as String)
         id("org.springframework.boot") version (extra["spring-boot.version"] as String)
         id("io.github.gradle-nexus.publish-plugin") version("1.1.0") apply(false)
+        id("com.bmuschko.docker-spring-boot-application") version (extra["bmuschko-docker-plugin.version"] as String)
     }
 }
